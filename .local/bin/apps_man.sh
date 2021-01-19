3f98573f5d8ecf47a96678e1aac0bb47e41cf003
KAlex-git <Kalash20150825@outlook.com>
2021-01-19 16:35:15 +0200

    no commentary

--- .local/bin/apps_man.sh
+++ .local/bin/apps_man.sh
@@ -9,31 +9,31 @@ local AWK
 AWK="$HOME/.config/install_system/awk/AWK_Essential_Training"
 case "$(printf "awk >>\\n01.Introduction/ >>\\n02.What Is AWK/ >>\\n03.AWK Command Line Basics/ >>\\n04.Understanding Records and Fields/ >>\\n05.Understanding Variables and Operators/ >>\\n06.A Quick Introduction to Regular Expressions/ >>\\n07.Using Control Structures/ >>\\n08.Formatting the Output/ >>\\n09.Functions and Arrays/ >>\\n10.Combining AWK with Other Tools/ >>\\n11.Conclusion/ >>\\nExercice Files/ >>" | dmenu -fn glisp -nb "#100" -nf "#b9c0af" -sb "#000" -sf "#afff2f" -i -l 22 -p "choice viewer: ")"  in
     "awk >>")
-	    ls $folder/awk/*.{pdf,docx}						| 2menu | xargs -I {} open "{}" ;;
+	    ls $folder/awk/*.{pdf,docx}																				| 2menu | xargs -I {} open "{}" ;;
     "01.Introduction/ >>")
-	    ls $AWK/01.\ Introduction/*						| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/01.\ Introduction/*																				| 2menu | xargs -I {} open "{}" ;;
     "02.What Is AWK/ >>")
-	    ls $AWK/02.\ What\ Is\ AWK/*					| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/02.\ What\ Is\ AWK/*																			| 2menu | xargs -I {} open "{}" ;;
     "03.AWK Command Line Basics/ >>")
-	    ls $AWK/03.\ AWK\ Command\ Line\ Basics/*				| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/03.\ AWK\ Command\ Line\ Basics/*													| 2menu | xargs -I {} open "{}" ;;
     "04.Understanding Records and Fields/ >>")
-	    ls $AWK/04.\ Understanding\ Records\ and\ Fields/*			| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/04.\ Understanding\ Records\ and\ Fields/*								| 2menu | xargs -I {} open "{}" ;;
     "05.Understanding Variables and Operators/ >>")
-	    ls $AWK/05.\ Understanding\ Variables\ and\ Operators/*		| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/05.\ Understanding\ Variables\ and\ Operators/*						| 2menu | xargs -I {} open "{}" ;;
     "06.A Quick Introduction to Regular Expressions/ >>")
-	    ls $AWK/06.\ A\ Quick\ Introduction\ to\ Regular\ Expressions/*	| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/06.\ A\ Quick\ Introduction\ to\ Regular\ Expressions/*		| 2menu | xargs -I {} open "{}" ;;
     "07.Using Control Structures/ >>")
-	    ls $AWK/07.\ Using\ Control\ Structures/*				| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/07.\ Using\ Control\ Structures/*													| 2menu | xargs -I {} open "{}" ;;
     "08.Formatting the Output/ >>")
-	    ls $AWK/08.\ Formatting\ the\ Output/* 				| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/08.\ Formatting\ the\ Output/*														| 2menu | xargs -I {} open "{}" ;;
     "09.Functions and Arrays/ >>")
-	    ls $AWK/09.\ Functions\ and\ Arrays/*				| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/09.\ Functions\ and\ Arrays/*															| 2menu | xargs -I {} open "{}" ;;
     "10.Combining AWK with Other Tools/ >>")
-	    ls $AWK/10.\ Combining\ AWK\ with\ Other\ Tools/*			| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/10.\ Combining\ AWK\ with\ Other\ Tools/*									| 2menu | xargs -I {} open "{}" ;;
     "11.Conclusion/ >>")
-	    ls $AWK/11.\ Conclusion/*						| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/11.\ Conclusion/*																					| 2menu | xargs -I {} open "{}" ;;
     "Exercice Files/ >>")
-	    ls $AWK/Exercice\ Files/*						| 2menu | xargs -I {} open "{}" ;;
+	    ls $AWK/Exercice\ Files/*																					| 2menu | xargs -I {} open "{}" ;;
 esac
 }
 
@@ -50,10 +50,10 @@ case "$choice" in
 	"Other ->") getkeys-dmenu;;
 	awk) awk;;
 	xargs) xar;;
-	"bash ->") 	ls $folder/SHELL/* | grep "bash*" 	| 2menu | xargs -I {} open "{}" ;;
-	"inkscape ->")	ls $folder/inkscape/*			| 2menu | xargs -I {} open "{}" ;;
-	"latex ->")	ls $folder/latex/*			| 2menu | xargs -I {} open "{}" ;;
-	"vim ->") 	ls $folder/vim/Documention/*    	| 2menu | xargs -I {} open "{}"  ;;
+	"bash ->") 	ls $folder/SHELL/* | grep "bash*"													| 2menu | xargs -I {} open "{}" ;;
+	"inkscape ->")	ls $folder/inkscape/*																	| 2menu | xargs -I {} open "{}" ;;
+	"latex ->")	ls $folder/latex/*																				| 2menu | xargs -I {} open "{}" ;;
+	"vim ->") 	ls $folder/vim/Documention/*															| 2menu | xargs -I {} open "{}"  ;;
 esac
 }
 
--- .local/bin/reserve_copy.sh
+++ .local/bin/reserve_copy.sh
@@ -3,68 +3,45 @@
 #source $HOME/.local/bin/shellColor.sh
 ##
 ##
-##--include - это то, что мы включаем в синхронизацию (в нашем случае все что начинается на n)
-##--exclude - это то, что мы исключаем (в нашем случае все остальное)
-##
-## rsync -avz --include 'n*' --exclude '*' root@192.168.0.5:/home/sysadmin/temp/ /home/greendail/temp
 
-input="$$HOME/.local/ $HOME/.config/ $HOME/vimwiki/"
-outputUSB="/mnt/usb/"
-
-
-# исключения
-share="$($HOME/.local/share/)"
-neverwinternight="$HOME/.config/install_system/neverwinternight_diamont/"
-
-	rsync -avzhv --exclude "$neverwinternight" --exclude "$share"  $input $outputUSB
-
-
-
-#[ -d /run/media/alex/ ] && echo "folder '/run/media/alex/ ' have " || sudo mkdir -p /run/media/alex/
-#sudo mount /dev/sda6 /run/media/alex/
 ### Sync folder "home"
-#    bkhome_sda6="/run/media/alex/alex/"
+sd="$(/mnt/usb/OS/Linux_OS/backup/alex)"
+
+# Folders
+sudo rsync -arvz --progress  "$HOME/Desktop"				"$sd/"
+sudo rsync -arvz --progress  "$HOME/Documents"			"$sd/"
+sudo rsync -arvz --progress  "$HOME/Downloads"			"$sd/"
+sudo rsync -arvz --progress  "$HOME/Pictures"			"$sd/"
+sudo rsync -arvz --progress  "$HOME/Music"					"$sd/"
+sudo rsync -arvz --progress  "$HOME/Videos"				"$sd/"
+sudo rsync -arvz --progress  "$HOME/www"						"$sd/"
+sudo rsync -arvz --progress  "$HOME/build"					"$sd/"
+sudo rsync -arvz --progress  "$HOME/dev"						"$sd/"
+sudo rsync -arvz --progress  "$HOME/GDisk"					"$sd/"
+sudo rsync -arvz --progress  "$HOME/SKE"						"$sd/"
+sudo rsync -arvz --progress  "$HOME/tmp"						"$sd/"
+sudo rsync -arvz --progress  "$HOME/vimwiki"				"$sd/"
+sudo rsync -arvz --progress  "$HOME/.dotfiles"			"$sd/"
+sudo rsync -arvz --progress  "$HOME/.config"				"$sd/"
+sudo rsync -arvz --progress  "$HOME/.local"				"$sd/"
+sudo rsync -arvz --progress  "$HOME/.abook"				"$sd/"
+sudo rsync -arvz --progress  "$HOME/.gnupg"				"$sd/"
+sudo rsync -arvz --progress  "$HOME/.npm"					"$sd/"
+sudo rsync -arvz --progress  "$HOME/.ssh"					"$sd/"
+sudo rsync -arvz --progress  "$HOME/.stardict"			"$sd/"
+sudo rsync -arvz --progress  "$HOME/.texlive"			"$sd/"
+sudo rsync -arvz --progress "$HOME/.fzf"						"$sd/"
+sudo rsync -arvz --progress "$HOME/.npm"						"$sd/"
+
+# Files
+sudo rsync -arvz --progress  "$HOME/.fzf.bash"			"$sd/"
+sudo rsync -arvz --progress  "$HOME/.fzf.zsh"			"$sd/"
+sudo rsync -arvz --progress  "$HOME/.gitconfig"		"$sd/"
+sudo rsync -arvz --progress  "$HOME/.gitmodules"		"$sd/"
+sudo rsync -arvz --progress  "$HOME/.mbsyncrc"			"$sd/"
+sudo rsync -arvz --progress  "$HOME/.xinitrc"			"$sd/"
+sudo rsync -arvz --progress  "$HOME/noiseclean.sh"	"$sd/"
+
 ##
 ##    ##folder {.config}
-#    rsync -avzur --delete  $bkhome_sda6/Desktop     		"$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/Documents  		"$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/Downloads  		"$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/MYSECRETDOC 		"$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/Pictures  		"$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/Music 			"$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/Videos  		"$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/www  			"$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/.dotfiles  		"$HOME/"
-##    rsync -avzur --delete --progress $bkhome_sda6/.cache      "$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/.config 		"$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/.icons  		"$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/.fonts  		"$HOME/"
-##    rsync -avzur --delete --progress $bkhome_sda6/.fzf        "$HOME/"
-##    rsync -avzur --delete --progress $bkhome_sda6/.npm        "$HOME/"
-##    rsync -avzur --delete --progress $bkhome_sda6/.local      "$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/.scripts  		"$HOME/"
-#    rsync -avzur --delete  $bkhome_sda6/.wine  			"$HOME/"
-#
-#    rsync -avur --delete  $bkhome_sda6/urls.txt			"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.bashrc 			"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.bash_completion		"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.inputrc			"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.gitignore		"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.gitconfig		"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.fzf.bash		"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.vimrc.bak		"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.tmux.conf  		"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.xbindkeysrc  		"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.Xdefaults 	        "$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.xinitrc 		"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.Xresources  		"$HOME/"
-#    rsync -avur --delete  $bkhome_sda6/.xvkbd  			"$HOME/"
-#
-#	## create link
-#	[ -f "$HOME/aliasrc" ] && echo "link have 'aliasrc'" || sudo ln -sf $HOME/.config/aliasrc $HOME/aliasrc
-#	[ -f "$HOME/.profile" ] && echo "link have '.profile'" || sudo ln -sf $HOME/.config/.profile $HOME/.profile
-#	[ -f "$HOME/.xprofile" ] && echo "link have '.xprofile'" || sudo ln -sf $HOME/.config/.profile $HOME/.xprofile
-#	[ -f "$HOME/windat" ] && echo "link have 'windat'" || sudo ln -sf "$HOME/.wine/drive_c/Program Files/Tracker Software/PDF Viewer/" $HOME/windat
-#	[ -d "$HOME/.vim" ] && echo "link have '.vim'" || sudo ln -sf $HOME/.config/.nvim $HOME/.vim
-#	[ -f "$HOME/.vimrc" ] && echo "link have '.vimrc'" || sudo ln -sf $HOME/.config/.nvim/init.vim $HOME/.vimrc
-#
+		#rsync -avzur --delete --progress "$HOME/.cache"			"$sd"
